8eb532702a4f7fcaffd7b1376e29d5999952bb30
vivek <vivek.gaud@routemobile.com>
2024-02-09 19:28:19 +0530

    url changes

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ app = Flask(__name__)
 dummy_data = []
 
 # Route to handle GET requests
-@app.route('/get_sip_stats', methods=['GET'])
+@app.route('/GetSipStats', methods=['GET'])
 def get_sip_stats():
     return jsonify(dummy_data)
 
 # Route to handle POST requests
-@app.route('/get_sip_stats', methods=['POST'])
+@app.route('/AddSipStats', methods=['POST'])
 def set_sip_stats():
     data = request.json  # Assuming JSON data is sent in the request body
     dummy_data.append(data)
